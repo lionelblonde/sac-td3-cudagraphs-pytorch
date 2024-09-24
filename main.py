c99cc44346e7a2d818184ba82395bdb9242a768f
lionelblonde <lionel.blonde@me.com>
2024-09-24 17:22:56 +0200

    Adjust some logging levels

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def get_name(uuid: str, env_id: str, seed: int) -> str:
 class MagicRunner(object):
 
     DISABLE_LOGGER: bool = False
-    LOGGER_LEVEL: int = logger.INFO
+    LOGGER_LEVEL: int = logger.WARN
     PRODUCTION_MODE: bool = True
 
     @beartype
