e9851b20144496b243502a57a3ca3cb9e23f380d
lionelblonde <lionel.blonde@me.com>
2024-09-26 10:30:40 +0200

    Update net sizes to make them smaller

--- agents/agent.py
+++ agents/agent.py
@@ -89,8 +89,8 @@ class Agent(object):
 
         # create online and target nets
 
-        actr_hid_dims = (400, 300)
-        crit_hid_dims = (400, 300)
+        actr_hid_dims = (256, 256)
+        crit_hid_dims = (256, 256)
 
         actr_net_args = [self.ob_shape, self.ac_shape, actr_hid_dims, self.rms_obs, self.max_ac]
         actr_net_kwargs = {"layer_norm": self.hps.layer_norm}
