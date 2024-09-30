e042d655e872c232a0c4f553c7ed0ad9f45d5272
lionelblonde <lionel.blonde@me.com>
2024-09-30 16:15:04 +0200

    Add comment to have a trace of the source of the bug

--- orchestrator.py
+++ orchestrator.py
@@ -324,6 +324,7 @@ def train(cfg: DictConfig,
 
             if agent.qnet_updates_so_far % cfg.actor_update_delay == 0:
                 # compensate for delay: wait X rounds, do X updates
+                # N.B.: WARNING -- any value >1 makes cudagraph not learn (why!?)
                 for _ in range(cfg.actor_update_delay):
                     # update actor (and alpha)
                     if eval_this_iter:
